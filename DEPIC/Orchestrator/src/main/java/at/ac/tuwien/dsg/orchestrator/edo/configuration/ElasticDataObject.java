5a76e509724f4b9cb752bd32cecbbf460cbc0617
jun-nguyen <d.nguyen@dsg.tuwien.ac.at>
2014-08-24 17:33:59 +0200

    Add Interface of Elastic Data Object

---
+++ DEPIC/Orchestrator/src/main/java/at/ac/tuwien/dsg/orchestrator/edo/configuration/ElasticDataObject.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package at.ac.tuwien.dsg.orchestrator.edo.configuration;
+
+/**
+ *
+ * @author Jun
+ */
+
+
+
+public interface ElasticDataObject {
+    List ListOfDataItems;
+    List ListOfDataElasticityMetrics;
+    
+    
+    
+}
