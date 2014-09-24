61ca15c5ab99a21a3b27ce26d51a9cd30f45bf18
jun-nguyen <d.nguyen@dsg.tuwien.ac.at>
2014-09-24 09:43:19 +0200

    clean example

---
+++ QoRAnalytics/src/main/java/at/ac/tuwien/dsg/qoranalytics/daw/test/demo.java
@@ -0,0 +1,12 @@
+package at.ac.tuwien.dsg.qoranalytics.daw.test;
+
+import at.ac.tuwien.dsg.qoranalytics.daw.engine.WorkflowEngine;
+
+public class demo {
+
+    public static final void main(String[] args) {
+        WorkflowEngine wfEngine = new WorkflowEngine("daw1");
+        wfEngine.startWFEngine();
+    }
+
+}
