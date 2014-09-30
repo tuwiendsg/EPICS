527ef66fba2fc3b7e93a1609a6d229346edeed8a
jun-nguyen <d.nguyen@dsg.tuwien.ac.at>
2014-09-30 10:42:04 +0200

    clean repo

--- DEPIC/README.md
+++
@@ -1,7 +0,0 @@
-DEPIC
-=====
-
-A Framework to Develop Elastic Data-as-a-Service
-
-
-
--- QoRAnalytics/src/main/java/at/ac/tuwien/dsg/qoranalytics/streamprocessing/entity/rule/StatementSubscriber.java
+++
@@ -1,8 +0,0 @@
-package at.ac.tuwien.dsg.qoranalytics.streamprocessing.entity.rule;
-
-
-public interface StatementSubscriber {
-
-    public String getStatement();
-
-}
