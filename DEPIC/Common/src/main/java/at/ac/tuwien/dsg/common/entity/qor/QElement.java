b66db8f5681be0fdf4e18c87d4904fbf41aa3633
jun-nguyen <d.nguyen@dsg.tuwien.ac.at>
2014-09-30 16:52:07 +0200

    update pom

--- DEPIC/Common/src/main/java/at/ac/tuwien/dsg/common/entity/qor/QElement.java
+++ DEPIC/Common/src/main/java/at/ac/tuwien/dsg/common/entity/qor/QElement.java
@@ -6,10 +6,61 @@
 
 package at.ac.tuwien.dsg.common.entity.qor;
 
+import java.util.List;
+
 /**
  *
  * @author Jun
  */
 public class QElement {
+    String qElementID;
+    List<MetricRange> listOfMetricRanges;
+    double price;
+    String unit;
+
+    public QElement() {
+    }
+
+    public QElement(String qElementID, List<MetricRange> listOfMetricRanges, double price, String unit) {
+        this.qElementID = qElementID;
+        this.listOfMetricRanges = listOfMetricRanges;
+        this.price = price;
+        this.unit = unit;
+    }
+
+    public String getqElementID() {
+        return qElementID;
+    }
+
+    public void setqElementID(String qElementID) {
+        this.qElementID = qElementID;
+    }
+
+    public List<MetricRange> getListOfMetricRanges() {
+        return listOfMetricRanges;
+    }
+
+    public void setListOfMetricRanges(List<MetricRange> listOfMetricRanges) {
+        this.listOfMetricRanges = listOfMetricRanges;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+    
+    
+    
     
 }
