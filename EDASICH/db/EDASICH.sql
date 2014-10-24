ad7a5292d674679520662cc7b7e8c4c2ecfa0e51
jun-nguyen <d.nguyen@dsg.tuwien.ac.at>
2014-10-24 14:59:16 +0200

    sample configure edasich

--- EDASICH/db/EDASICH.sql
+++ EDASICH/db/EDASICH.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50521
  File Encoding         : utf-8
 
- Date: 10/20/2014 09:50:24 AM
+ Date: 10/24/2014 14:38:42 PM
 */
 
 SET NAMES utf8;
@@ -43,16 +43,11 @@ DROP TABLE IF EXISTS `Daf`;
 CREATE TABLE `Daf` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
   `status` varchar(255) DEFAULT NULL,
+  `file` mediumblob,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
-
--- ----------------------------
---  Records of `Daf`
--- ----------------------------
-BEGIN;
-INSERT INTO `Daf` VALUES ('1', 'daf1', 'stop');
-COMMIT;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 --  Table structure for `Event`
@@ -65,13 +60,6 @@ CREATE TABLE `Event` (
   `event_values` varchar(255) DEFAULT NULL,
   `severity` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
-
--- ----------------------------
---  Records of `Event`
--- ----------------------------
-BEGIN;
-INSERT INTO `Event` VALUES ('1', 'daf1', 'Thu 2014.10.09 at 02:35:59 AM UTC', '[SensorY  - Value: 18699.6583591] ; [SensorY  - Value: 16088.9995849] ; ', 'EMERGENCY'), ('2', 'daf1', 'Thu 2014.10.09 at 02:36:34 AM UTC', '[SensorY  - Value: 18523.2182418] ; [SensorY  - Value: 10249.5530907] ; [SensorY  - Value: 18131.0640982] ; ', 'WARNING'), ('3', 'daf1', 'Thu 2014.10.09 at 02:36:34 AM UTC', '[SensorY  - Value: 18131.0640982] ; [SensorY  - Value: 18523.2182418] ; ', 'EMERGENCY');
-COMMIT;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 SET FOREIGN_KEY_CHECKS = 1;
