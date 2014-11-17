7a4a43cfa060e3cdf37b8b4261e8a647a704be51
jun-nguyen <d.nguyen@dsg.tuwien.ac.at>
2014-11-17 14:14:54 +0100

    tp monitor

---
+++ Utils/MonitoringServices/ThroughputMonitor/src/main/java/at/ac/tuwien/dsg/throughputmonitor/service/ThroughputMonitorService.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.ac.tuwien.dsg.throughputmonitor.service;
+
+import at.ac.tuwien.dsg.throughputmonitor.algorithm.ThroughputMonitor;
+
+/**
+ *
+ * @author Jun
+ */
+public class ThroughputMonitorService {
+    
+    public String requestThroughputMonitorService(String daID){
+        
+        ThroughputMonitor throughputMonitor = new ThroughputMonitor();
+        double throughput = throughputMonitor.monitorThroughput(daID);
+        
+        return String.valueOf(throughput);
+    }
+    
+}
