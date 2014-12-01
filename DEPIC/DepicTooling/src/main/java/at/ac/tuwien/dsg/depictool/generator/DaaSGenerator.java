a80caea6987cb2c30c67b0d1efc21a159da1441b
jun-nguyen <d.nguyen@dsg.tuwien.ac.at>
2014-12-01 14:09:28 +0100

    salsa connector

--- DEPIC/DepicTooling/src/main/java/at/ac/tuwien/dsg/depictool/generator/DaaSGenerator.java
+++ DEPIC/DepicTooling/src/main/java/at/ac/tuwien/dsg/depictool/generator/DaaSGenerator.java
@@ -58,6 +58,8 @@ public class DaaSGenerator {
         generateMetricConstraintClass();
         generateConstraintConverterClass();
         generateConsumerRequirementClass();
+        
+        
 
     }
     
